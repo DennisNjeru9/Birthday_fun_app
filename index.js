b9029996e65e308e362cee46d4faac411202aa9a
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-06-14 12:56:48 +0300

    add maleAkan and femaleAkan variables to the JS file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let yearOfBirth = document.querySelector("#ybirth").value;
 let monthOfBirth = document.quertSelector("#mbrith").value;
 let dateOfBirth = document.querySelector("#dbirth").value;
+
+let maleAkan = [{day:"Sunday", name:"Kwasi"},{day:"Monday", name:"Kwadwo"},{day:"Tuesday", name:"Kwabena"},{day:"Wednesday", name:"Kwaku"},{day:"Thursday", name:"Yaw"}, {day:"Friday", name:"Kofi"},{day:"Saturday", name:"Kwame"}];
+let femaleAkan = [{day:"Sunday", name:"Akosua"},{day:"Monday", name:"Adwoa"},{day:"Tuesday", name:"Abenaa"},{day:"Wednesday", name:"Akua"},{day:"Thursday", name:"Yaa"}, {day:"Friday", name:"Afua"},{day:"Saturday", name:"Ama"}]
